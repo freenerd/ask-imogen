70a929d463deecf33e6b04044253f2b54f280897
Freenerd <johan@soundcloud.com>
2011-01-23 22:10:13 +0100

    More recbutton copy

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  def index
+    render :layout => false
+  end
+
+  #def check_track
+    ## params[:track_uri]
+    #render :text => session
+  #end
+
+  #def login
+    #if session[:access_token] && session[:access_token_secret]
+      #redirect_to :action => 'index'
+    #end
+  #end
+
+  #def logout
+    #session[:access_token] = nil
+    #session[:access_token_secret] = nil
+    #redirect_to :action => 'index'
+  #end
+end
+
