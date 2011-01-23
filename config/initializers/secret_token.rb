f3813d4a52e2769c36e4e189b107071001c77042
Freenerd <johan@soundcloud.com>
2011-01-23 12:17:33 +0100

    Starting Rails3 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AskImogen::Application.config.secret_token = 'b575c627006b24feead31690d63a4dd7418632ce960505ea93f44e6d070a3b42a5ccfc7a09223c8d328e506a4da926055712863b2a6fafe5b39b0a31d5b4a7e3'
