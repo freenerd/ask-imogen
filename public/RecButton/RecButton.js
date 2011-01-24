f9cf323cad2c8924532f416d64aff26cdfd7190b
Eric Wahlforss <eric@Forss.local>
2011-01-24 10:40:04 +0100

    adding new recbtn

---
+++ public/RecButton/RecButton.js
@@ -0,0 +1,92 @@
+var RecButton = {
+  swfCode: '<object width="230" height="140"><param value="transparent" name="wmode"><param value="Recorder.swf" name="movie"><param value="always" name="allowscriptaccess"><embed width="230" height="140" wmode="transparent"   type="application/x-shockwave-flash" src="Recorder.swf" allowscriptaccess="always">  </object>',
+  swfObject: null,
+  events: {},
+    //showFlash, hideFlash, recordStart, recordStop:, playStart: , playStop:  , uploadStart, uploadState, uploadStop:, error:     
+  setup: function(options){
+    this.bind('showFlash', options['onShowFlash']);
+    this.bind('hideFlash', options['onHideFlash']);
+    options['flashContainer'].innerHTML = this.swfCode;
+    this.swfObject = options['flashContainer'].children[0];
+  },
+
+  clear: function(){
+    RecButton.events = {};
+  },
+
+  record: function(options){
+    this.bind('recordingStart', options['onStart']);
+    this.flashInterface().startRecording();
+  },
+  
+  stop: function(){
+    this.flashInterface().stopRecording();
+    this.flashInterface().stopPlaying();
+  },
+  
+  play: function(){
+    this.flashInterface().startPlaying();
+  },
+
+  upload: function(url, params){
+    this.flashInterface().upload(url, params);
+  },
+
+  bind: function(eventName, fn){
+    if(!RecButton.events[eventName]){ RecButton.events[eventName] = [] }
+    RecButton.events[eventName].push(fn);
+  },
+  
+  trigger: function(eventName, args){
+    for(var cb in RecButton.events[eventName]){
+      RecButton.events[eventName][cb](args);
+    }
+  },
+
+  flashInterface: function(){
+    return this.swfObject.sendToActionScript ? this.swfObject : this.swfObject.children[3];
+  },
+};
+
+
+function Recorder(swfObject){
+   this.swfObject   = swfObject;
+   this.isPlaying   = false;
+   this.isRecording = false;
+   
+   this.send = function(message){
+     return swfObject.sendToActionScript(message);
+   };
+   
+   this.upload = function(uri){
+     return this.send('upload ' + uri);
+   };
+   
+   this.setup = function(){
+     return this.send('setup');
+   };
+   
+   this.startRecording = function(){
+     this.isRecording = true;
+     return this.send('startRecording');
+   };
+   
+   this.stopRecording = function(){
+     this.isRecording = false;
+     return this.send('stopRecording');
+   };
+   
+   this.startPlaying = function(){
+     this.isPlaying = true;
+     return this.send('startPlaying');
+   };
+   
+   this.stopPlaying = function(){
+     this.isPlaying = false;
+     return this.send('stopPlaying');
+   };
+   
+   this.getDebugLog = function(){
+     return this.send('getDebugLog');
+   };
+}
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,85 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function debug(msg){
+  console.log(msg);
+}
+
+function timer(node){
+  node.innerHTML = "0:00"
+  node.timer = window.setInterval(function(){
+    var tmp = node.innerHTML.split(":");
+    var time = parseInt(tmp[0])*60 + parseInt(tmp[1]);
+    time += 1;
+    node.innerHTML = parseInt(time / 60) + ":" + time % 60;
+  }, 1000);
+}
+
+$(function(){
+  RecButton.bind('debug', function(arg){
+    debug('DEBUG: ' + arg.message);
+  });
+
+
+  $('.RecButtonWidget .button').live('click', function(){
+    console.log($(this).attr('class'));
+  });
+
+
+  RecButton.setup({
+    flashContainer: $('.RecButtonRecorder')[0],
+    onShowFlash: function(){
+      $('.RecButtonRecorder').addClass('highlight');
+    },
+    onHideFlash: function(arg){
+      $('.RecButtonRecorder').removeClass('highlight');
+    }
+  });
+
+  $('.RecButtonWidget.recording .button.record').live('click', function(){
+    RecButton.stop();
+    $RecButtonWidget = $(this).closest('.RecButtonWidget');
+    $RecButtonWidget.removeClass('recording').addClass("recorded");
+    window.clearInterval($RecButtonWidget.find('.timer')[0].timer);
+    return false;
+  });
+  
+  $('.RecButtonWidget .button.record').live('click', function(){
+    var that = this;
+    RecButton.clear();
+    RecButton.record({
+      onStart: function(){
+        $RecButtonWidget = $(that).closest('.RecButtonWidget');
+        $RecButtonWidget.removeClass("recorded").addClass('recording');
+        timer($RecButtonWidget.find('.timer')[0]);
+      }
+    });
+  });
+
+  // play
+
+  $('.RecButtonWidget.playing .button.play').live('click', function(){
+    RecButton.stop();
+    $RecButtonWidget = $(this).closest('.RecButtonWidget');
+    $RecButtonWidget.removeClass('playing').addClass("played");
+    //    window.clearInterval($RecButtonWidget.find('.timer')[0].timer);
+    return false;
+  });
+
+  $('.RecButtonWidget .button.play').live('click', function(){
+    var that = this;
+    RecButton.play();
+    $RecButtonWidget = $(that).closest('.RecButtonWidget');
+    $RecButtonWidget.removeClass("played").addClass('playing');
+  });
+  
+  
+  $('.scconnect').live('click', function(){
+    var url = 'http://localhost/complete.html';
+//    var rt_url = 'http://recbutton.com/oauth/request_token?next=' + url;
+    // returns with 
+    var rt_url = 'http://localhost:3002/oauth/request_token?next=' + url;
+    
+    var x = window.open(rt_url, "sc_connect_popup", "location=1, width=456, height=500,toolbar=no,scrollbars=yes");
+  });
+
+});
+
+var x;
